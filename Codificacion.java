9ef695a8ba09532293b3c01cdadd771c40e04e6f
Andrea Mourino <amourinodesousa@danielcastelao.org>
2024-12-13 09:45:47 +0100

    crear tabla

---
+++ Codificacion.java
@@ -0,0 +1,20 @@
+public class Codificacion {
+}
+public class Damas {
+    public static void main(String[] args) {
+        // Representación del tablero:
+        // 'B' = ficha del jugador blanco
+        // 'N' = ficha del jugador negro
+        // ' ' = casilla vacía
+        char[][] tablero = {
+                {'B', ' ', 'B', ' ', 'B', ' ', 'B', ' '},
+                {' ', 'B', ' ', 'B', ' ', 'B', ' ', 'B'},
+                {'B', ' ', 'B', ' ', 'B', ' ', 'B', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                {' ', 'N', ' ', 'N', ' ', 'N', ' ', 'N'},
+                {'N', ' ', 'N', ' ', 'N', ' ', 'N', ' '},
+                {' ', 'N', ' ', 'N', ' ', 'N', ' ', 'N'}
+        };
+
+
